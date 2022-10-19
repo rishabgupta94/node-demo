86cbbf89c614751551875545c2c2d8f618887291
Rishab Gupta <rishabgupta98@gmail.com>
2022-10-19 16:23:39 -0700

    uses env

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
 const Joi = require('joi');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
 
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
+    { id: 3, name: 'course3' },
 ];
 
 app.get('/', (req, res) => {
